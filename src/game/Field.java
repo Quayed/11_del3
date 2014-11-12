beeb0ba89f73494884d19ba797359d5924c81829
Mathias Sondrup <matsondrup@gmail.com>
2014-11-12 19:37:04 +0100

    Added name att and getName, as all fields have names. Also added fieldId and getFieldId as all fields have Ids.

--- src/game/Field.java
+++ src/game/Field.java
@@ -1,5 +1,15 @@
 package game;
 
 abstract public class Field {
+	protected String name;
+	protected int fieldId;
 	abstract public void landOnField(Player player);
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public int getFieldId(){
+		return this.fieldId;
+	}
 }
