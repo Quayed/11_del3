a73af2532ef97b77f4a02d7f2fb529e697494d0f
Mathias Sondrup <matsondrup@gmail.com>
2014-11-12 15:29:35 +0100

    Inserted code from player class in CDIO 2.

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,5 +1,32 @@
 package game;
 
 public class Player {
-
+	private String name;
+	private int field;
+	final private int ID;
+	
+	public Player(int id, String name){
+		this.name = name;
+		this.ID = id;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getName(String name){
+		return name;
+	}
+	
+	public int getId(){
+		return ID;
+	}
+	
+	public void setField(int field){
+		this.field = field;
+	}
+	
+	public int getField(){
+		return this.field;
+	}
 }
