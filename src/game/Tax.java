0e18fa6d1d3682c5b0bdb92ce75dacb83fdfccc5
Mathias Sondrup <matsondrup@gmail.com>
2014-11-12 15:16:26 +0100

    Added contructor

--- src/game/Tax.java
+++ src/game/Tax.java
@@ -4,6 +4,10 @@ public class Tax extends Field{
 	private int taxAmount;
 	private int taxRate;
 	
+	public Tax(int taxAmount){
+		this.taxAmount = taxAmount;
+	}
+	
 	@Override
 	public void landOnField(Player player) {
 		// felt 16, spilleren skal miste 2000
