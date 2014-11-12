bef05524a477f14c9d56fd491ad1eb0b99ff2258
Mathias Sondrup <matsondrup@gmail.com>
2014-11-12 13:32:24 +0100

    Created the die class. Same code as used in CDIO 2.

--- src/game/Die.java
+++ src/game/Die.java
@@ -1,5 +1,20 @@
 package game;
+import java.util.Random;
 
 public class Die {
-
+	private int faceValue;
+	private Random rnd = new Random();
+	
+	public int roll(){
+		faceValue = rnd.nextInt(6)+1;
+		return faceValue;
+	}
+	
+	public void setDie(int value){
+		faceValue = value;
+	}
+	
+	public int getDie(){
+		return faceValue;
+	}
 }
