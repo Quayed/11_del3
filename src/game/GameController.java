9ee65630c0ac1bbc307d6b2df5a235dfd9063704
adamsen9 <adamsen9@gmail.com>
2014-11-14 10:51:37 +0100

    Renamed Game -> GameController

---
+++ src/game/GameController.java
@@ -0,0 +1,27 @@
+package game;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+public class GameController {
+
+	public static void main(String[] args) {
+		Die dice = new Die();
+		int players;
+		//Vælg antal spillere
+		players = 3;
+		//Kør spillet
+		GameBoard Board =  new GameBoard();
+		
+		//Opsætning af bordet
+		
+		//Første terningekast
+		
+		
+		
+		
+		
+		
+	}
+
+}
