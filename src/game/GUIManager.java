0dc4cf52a5944c3a7380cd13a24a2272a8db5265
nicklas669 <nicklas669@gmail.com>
2014-11-14 12:05:44 +0100

    Added create() and addPlayer() to GUIManager.

--- src/game/GUIManager.java
+++ src/game/GUIManager.java
@@ -1,5 +1,18 @@
 package game;
 
-public class GUIManager {
+import boundaryToMatador.GUI;
 
+public class GUIManager {
+	
+	public static void create() {
+		GUI.create("fields.txt");
+	}
+	
+	public static void addPlayer(String name, int balance) {
+		GUI.addPlayer(name, balance);
+	}
+	
+	
 }
+
+
