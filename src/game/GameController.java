4ddb358621c5e5d0771ea3604ddada86f9302be5
nicklas669 <nicklas669@gmail.com>
2014-11-14 12:12:29 +0100

    Added 2 players and calls to GUIManager. Now have showing GUI using fields.txt for fields.

--- src/game/GameController.java
+++ src/game/GameController.java
@@ -8,12 +8,18 @@ public class GameController {
 	public void run() {
 		Die dice = new Die();
 		int players;
+		
 		//Vælg antal spillere
-		players = 3;
+		players = 2;
+		Player player1 = new Player(1, "Jens");
+		Player player2 = new Player(2, "Jens2");
+
 		//Kør spillet
 		GameBoard Board =  new GameBoard();
 		
 		//Opsætning af bordet
+		GUIManager.create();
+		GUIManager.addPlayer("JENS", 1000);
 		
 		//Første terningekast
 		
