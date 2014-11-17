cade4d0950131a793750a213f6ed8b2d6e7bf6f2
nicklas669 <nicklas669@gmail.com>
2014-11-17 21:37:48 +0100

    Removed Account[6] array, every Player object now has it's own Account created upon instantiation. Retrievable with Player.getAcc().*

--- src/game/GameController.java
+++ src/game/GameController.java
@@ -11,7 +11,6 @@ public class GameController {
 	public void run() {
 		Die dice = new Die();
 		Player players[] = new Player[6];
-		Account accounts[] = new Account[6];
 		int numberOfPlayers;
 		int turn = 0; 
 		int dieOne, dieTwo, field;
@@ -28,8 +27,7 @@ public class GameController {
 		for(int i = 0; i < numberOfPlayers; i++){
 			// her bør der tilføjes muligheden for spillerne at vælge navn - dette skal dog først gøres til sidst.
 			players[i] = new Player(i, "Spiller"+i);
-			accounts[i] = players[i].getAcc();
-			display.addPlayer(players[i].getName(), accounts[i].getBalance());
+			display.addPlayer(players[i].getName(), players[i].getAcc().getBalance());
 		}
 		
 		//Første terningekast
