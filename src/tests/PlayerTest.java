15ae0cc71156e68b3704e19ba7bbea4f134d57c3
RobertEriksen <Robert.b.e@hotmail.com>
2014-11-24 16:06:13 +0100

    Har lavet nogle assertequals i PlayerTest

--- src/tests/PlayerTest.java
+++ src/tests/PlayerTest.java
@@ -3,12 +3,16 @@ package tests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
+import game.*;
 public class PlayerTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//Vi starter med at oprette en spiller med navnet "THOMAS" og ID 101. Derefter ser vi om THOMAS har de rigtige startegenskaber
+		Player player = new Player(101, "THOMAS");
+		assertEquals(player.getName(),"THOMAS");
+		assertEquals(player.getId(),101);
+		assertEquals(player.getStartMoney(),5000);
 	}
 
 }
