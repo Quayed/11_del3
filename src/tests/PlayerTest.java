d62eea68c6a2a45f86a848e3ddc6eabd44f39c8a
RobertEriksen <Robert.b.e@hotmail.com>
2014-11-25 14:37:11 +0100

    Victor og Robert har testet playerclassen med j-unit test. Blackbox metode.

--- src/tests/PlayerTest.java
+++ src/tests/PlayerTest.java
@@ -8,7 +8,7 @@ public class PlayerTest {
 
 	@Test
 	public void test() {
-		//Vi starter med at oprette en spiller med navnet "THOMAS" og ID 101. Derefter ser vi om THOMAS har de rigtige startegenskaber
+		//Test af start egenskab:
 		Player player = new Player(101, "THOMAS");
 		assertEquals(player.getName(),"THOMAS");
 		assertEquals(player.getId(),101);
@@ -16,18 +16,23 @@ public class PlayerTest {
 		assertEquals(player.getNumberOfFleetsOwned(),0);
 		assertEquals(player.getNumberOfLaborCampsOwned(),0);
 		assertEquals(player.getNumberOfFieldsOwned(),0);
+		
+		//Vi tester add og set metoderne, for felterne.
 		player.addNumberOfFleetsOwned();
 		player.addNumberOfLaborCamps();
-		player.addToInventory(1);
+		player.addToInventory(10);
 		assertEquals(player.getNumberOfFleetsOwned(),1);
 		assertEquals(player.getNumberOfLaborCampsOwned(),1);
 		assertEquals(player.getNumberOfFieldsOwned(),1);
+		assertEquals(player.getInventory()[0],10);
+		
+		//Vi tester om metoden Haslost, setHasLost og isHasLost virker.
 		assertEquals(player.isHasLost(),false);
 		player.setHasLost(true);		
 		assertEquals(player.isHasLost(),true);
 		
 		
-		
+		//VI tester Field og move metoderne virker.
 		assertEquals(player.getField(),0);
 		player.move(6);
 		assertEquals(player.getField(),6);
@@ -35,12 +40,7 @@ public class PlayerTest {
 		assertEquals(player.getField(),21);
 		player.move(12);
 		assertEquals(player.getField(),12);
-		
-		
-		
-		
-		
-		
+		assertEquals(player.getPrevField(),21);		
 		
 	}
 
