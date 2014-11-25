1b6cbb3e0f08df051c29bfd59aa55018740e47f7
RobertEriksen <Robert.b.e@hotmail.com>
2014-11-25 15:00:04 +0100

    Har intenderet og lavet alle testene om til metoder

--- src/tests/PlayerTest.java
+++ src/tests/PlayerTest.java
@@ -7,7 +7,7 @@ import game.*;
 public class PlayerTest {
 
 	@Test
-	public void test() {
+	public void TestInitialConditions() {
 		//Test af start egenskab:
 		Player player = new Player(101, "THOMAS");
 		assertEquals(player.getName(),"THOMAS");
@@ -16,8 +16,12 @@ public class PlayerTest {
 		assertEquals(player.getNumberOfFleetsOwned(),0);
 		assertEquals(player.getNumberOfLaborCampsOwned(),0);
 		assertEquals(player.getNumberOfFieldsOwned(),0);
-		
-		//Vi tester add og set metoderne, for felterne.
+	}
+	
+	@Test
+	//Vi tester add og set metoderne, for felterne.
+	public void testAddSet()  {
+		Player player = new Player(101, "THOMAS");
 		player.addNumberOfFleetsOwned();
 		player.addNumberOfLaborCamps();
 		player.addToInventory(10);
@@ -25,14 +29,21 @@ public class PlayerTest {
 		assertEquals(player.getNumberOfLaborCampsOwned(),1);
 		assertEquals(player.getNumberOfFieldsOwned(),1);
 		assertEquals(player.getInventory()[0],10);
-		
-		//Vi tester om metoden Haslost, setHasLost og isHasLost virker.
+	}
+	
+	@Test
+	//Vi tester om metoden Haslost, setHasLost og isHasLost virker.
+	public void testLost(){
+		Player player = new Player(101, "THOMAS");
 		assertEquals(player.isHasLost(),false);
 		player.setHasLost(true);		
 		assertEquals(player.isHasLost(),true);
-		
-		
-		//VI tester Field og move metoderne virker.
+	}
+
+	@Test
+	//VI tester Field og move metoderne virker.
+	public void testFieldmove() {
+		Player player = new Player(101, "THOMAS");
 		assertEquals(player.getField(),0);
 		player.move(6);
 		assertEquals(player.getField(),6);
@@ -41,7 +52,8 @@ public class PlayerTest {
 		player.move(12);
 		assertEquals(player.getField(),12);
 		assertEquals(player.getPrevField(),21);		
-		
 	}
+	
+
 
 }
