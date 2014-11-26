dc404021c115b922ea50379b7085725f3d61f7ee
RobertEriksen <Robert.b.e@hotmail.com>
2014-11-26 18:56:19 +0100

    Har oprettet endnu en konstruktør så jeg kan kører tests

--- src/game/Die.java
+++ src/game/Die.java
@@ -4,12 +4,22 @@ import java.util.Random;
 public class Die {
 	private int faceValue;
 	private Random rnd = new Random();
+	String state;
+	int tal;
+	
+	public Die(int tal, String test){
+		this.tal = tal;
+		this.state = test;
+	}
 	
 	public Die(){
 		faceValue = 0;
 	}
 	
 	public int roll(){
+		if (state == "test"){
+				return tal;
+		}
 		faceValue = rnd.nextInt(6)+1;
 		return faceValue;
 	}
