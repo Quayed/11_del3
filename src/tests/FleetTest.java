761e77047d46ba83fd47c854a6d973380c00fd84
Mathias Sondrup <matsondrup@gmail.com>
2014-11-26 22:59:40 +0100

    Renamed the test file for fleets to FleetTest.

--- src/tests/FleetTest.java
+++ src/tests/FleetTest.java
@@ -5,19 +5,12 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class Fleetttests {
+public class FleetTest {
 
 	
 	//Kontrol af hvorvidt det rigtige beløb bliver trukket fra idet en fleet købes
 	@Test
 	public void buyTest() {
 		
-	}
-	
-	
-	//Kontrol af hvorvidt det rigtige beløb bliver trukket fra idet man land på en fleet
-	@Test
-	public void payRentTest() {
-		fail("Not yet implemented");
 	}
 }
