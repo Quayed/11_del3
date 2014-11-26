a4be75543382611c3b551daeff29083845fae266
Mathias Sondrup <matsondrup@gmail.com>
2014-11-26 12:49:17 +0100

    Moved main method to GameController and deleted Main.java

--- src/controllers/GameController.java
+++ src/controllers/GameController.java
@@ -25,6 +25,11 @@ public class GameController {
 		
 	}
 	
+	public static void main(String[] args) {
+		GameController game = new GameController();
+		game.run();
+	}
+	
 	public void run() {
 		board = new GameBoard();
 		dice = new Die();
