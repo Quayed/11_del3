4ff9b39b0d15f5f0658b4b615218d0ccb44968ed
Mathias Sondrup <matsondrup@gmail.com>
2014-11-28 22:28:49 +0100

    Put parameters of assertEqual into the correct order.

--- src/tests/PlayerTest.java
+++ src/tests/PlayerTest.java
@@ -10,12 +10,12 @@ public class PlayerTest {
 	public void TestInitialConditions() {
 		//Test af start egenskab:
 		Player player = new Player(101, "THOMAS");
-		assertEquals(player.getName(),"THOMAS");
-		assertEquals(player.getId(),101);
-		assertEquals(player.getBalance(),30000);
-		assertEquals(player.getNumberOfFleetsOwned(),0);
-		assertEquals(player.getNumberOfLaborCampsOwned(),0);
-		assertEquals(player.getNumberOfFieldsOwned(),0);
+		assertEquals("THOMAS", player.getName());
+		assertEquals(101, player.getId());
+		assertEquals(30000, player.getBalance());
+		assertEquals(0, player.getNumberOfFleetsOwned());
+		assertEquals(0, player.getNumberOfLaborCampsOwned());
+		assertEquals(0, player.getNumberOfFieldsOwned());
 	}
 	
 	@Test
@@ -25,33 +25,33 @@ public class PlayerTest {
 		player.addNumberOfFleetsOwned();
 		player.addNumberOfLaborCamps();
 		player.addToInventory(10,1000);
-		assertEquals(player.getNumberOfFleetsOwned(),1);
-		assertEquals(player.getNumberOfLaborCampsOwned(),1);
-		assertEquals(player.getNumberOfFieldsOwned(),1);
-		assertEquals(player.getInventory()[0],10);
+		assertEquals(1, player.getNumberOfFleetsOwned());
+		assertEquals(1,player.getNumberOfLaborCampsOwned());
+		assertEquals(1, player.getNumberOfFieldsOwned());
+		assertEquals(10, player.getInventory()[0]);
 	}
 	
 	@Test
 	//Vi tester om metoden Haslost, setHasLost og isHasLost virker.
 	public void testLost(){
 		Player player = new Player(101, "THOMAS");
-		assertEquals(player.isHasLost(),false);
+		assertEquals(false, player.isHasLost());
 		player.setHasLost(true);		
-		assertEquals(player.isHasLost(),true);
+		assertEquals(true, player.isHasLost());
 	}
 
 	@Test
 	//VI tester Field og move metoderne virker.
 	public void testFieldmove() {
 		Player player = new Player(101, "THOMAS");
-		assertEquals(player.getField(),0);
+		assertEquals(0, player.getField());
 		player.move(6);
-		assertEquals(player.getField(),6);
+		assertEquals(6, player.getField());
 		player.move(15);
-		assertEquals(player.getField(),21);
+		assertEquals(21, player.getField());
 		player.move(12);
-		assertEquals(player.getField(),12);
-		assertEquals(player.getPrevField(),21);		
+		assertEquals(12, player.getField());
+		assertEquals(21, player.getPrevField());		
 	}
 	
 
